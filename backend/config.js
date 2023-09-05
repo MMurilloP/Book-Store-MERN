ecd2d36bce09552a8a5bc03f3fecfd89790302d2
Manuel Murillo <mmpeira@gmail.com>
2023-09-05 11:32:03 +0200

    conectado a mongo y server arrancado

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5000;
+
+export const mongoDBURL =
+  "mongodb+srv://pruebamongodb2:xIFB97tV02qYtn5l@cluster0.jlsvywv.mongodb.net/?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Hello World");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
