9d8d88fb64288bfbdee9fe4a1fcfd444a22f4bbd
Manuel Murillo <mmpeira@gmail.com>
2023-09-06 11:44:39 +0200

    .env added

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
-export const PORT = 5000;
+import dotenv from "dotenv";
+dotenv.config();
 
-export const mongoDBURL =
-  "mongodb+srv://pruebamongodb2:xIFB97tV02qYtn5l@cluster0.jlsvywv.mongodb.net/?retryWrites=true&w=majority";
+export const PORT = process.env.PORT;
+export const mongoDBURL = process.env.mongoDBURL;
