11ebc6b6ed275cacfea90df5d9b5fa687b6ad2e2
Manuel Murillo <mmpeira@gmail.com>
2023-09-07 10:39:03 +0200

    deploy4

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
 import cors from "cors";
 import dotenv from "dotenv";
+// import path from "path";
 dotenv.config();
 
 const app = express();
@@ -17,6 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/books", booksRoute);
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.resolve(__dirname, "../client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+//   });
+// }
 
 mongoose
   .connect(mongoDBURL)
