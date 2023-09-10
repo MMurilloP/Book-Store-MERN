46718ff621745d992a6d3c6a93e3c41fa09e2c02
Manuel Murillo <mmpeira@gmail.com>
2023-09-10 20:18:02 +0200

    deploy10

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,9 @@ app.use(cors());
 app.use("/books", booksRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
